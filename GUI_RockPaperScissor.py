f54b2d350ee098ae0acae941ce14ad036b4fc866
Praveena Acharya <pravinaacharya2000@gmail.com>
2020-04-30 17:02:25 +0530

    GUI without score: one function

--- GUI_RockPaperScissor.py
+++ GUI_RockPaperScissor.py
@@ -4,37 +4,34 @@ import random
 window = tkinter.Tk()
 
 
-def rock():
+def compute(user_input):
     computer_input = randomChoice()
-    if computer_input == 'ROCK':
-        result.configure(text=" TIE")
-    elif computer_input == 'PAPER':
-        result.configure(text="YOU LOSE")
+    if user_input == 'R':
+        if computer_input == 'ROCK':
+            result.configure(text=" TIE")
+        elif computer_input == 'PAPER':
+            result.configure(text="YOU LOSE")
+        else:
+            result.configure(text="YOU WON")
+        choice.configure(text="Computer chose : " + computer_input)
+
+    elif user_input == 'P':
+        if computer_input == 'ROCK':
+            result.configure(text="YOU WON")
+        elif computer_input == 'PAPER':
+            result.configure(text=" TIE")
+        else:
+            result.configure(text="YOU LOSE")
+        choice.configure(text="Computer chose : " + computer_input)
+
     else:
-        result.configure(text="YOU WON")
-    choice.configure(text="Computer chose : " + computer_input)
-
-
-def paper():
-    computer_input = randomChoice()
-    if computer_input == 'ROCK':
-        result.configure(text="YOU WON")
-    elif computer_input == 'PAPER':
-        result.configure(text=" TIE")
-    else:
-        result.configure(text="YOU LOSE")
-    choice.configure(text="Computer chose : " + computer_input)
-
-
-def scissor():
-    computer_input = randomChoice()
-    if computer_input == 'ROCK':
-        result.configure(text="YOU LOSE")
-    elif computer_input == 'PAPER':
-        result.configure(text="YOU WON")
-    else:
-        result.configure(text=" TIE")
-    choice.configure(text="Computer chose : " + computer_input)
+        if computer_input == 'ROCK':
+            result.configure(text="YOU LOSE")
+        elif computer_input == 'PAPER':
+            result.configure(text="YOU WON")
+        else:
+            result.configure(text=" TIE")
+        choice.configure(text="Computer chose : " + computer_input)
 
 
 def randomChoice():
@@ -46,13 +43,13 @@ def randomChoice():
 MyTitle = tkinter.Label(window, text="ROCK PAPER SCISSOR", font="Helvetica 10 ")
 MyTitle.pack()
 
-MyButton = tkinter.Button(window, text="Rock", command=rock)
+MyButton = tkinter.Button(window, text="Rock", command=lambda: compute('R'))
 MyButton.pack()
 
-Button2 = tkinter.Button(window, text="Paper", command=paper)
+Button2 = tkinter.Button(window, text="Paper", command=lambda: compute('P'))
 Button2.pack()
 
-Button3 = tkinter.Button(window, text="Scissor", command=scissor)
+Button3 = tkinter.Button(window, text="Scissor", command=lambda: compute('S'))
 Button3.pack()
 
 choice = tkinter.Label(window, font="Helvetica 10")
